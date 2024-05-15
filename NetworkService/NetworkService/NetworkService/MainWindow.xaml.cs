9901e7f8be96285aa7be464328bbd0d525972015
Dragan <milosevicdragan002@gmail.com>
2024-05-15 12:30:46 +0200

    Design for app

--- NetworkService/NetworkService/NetworkService/MainWindow.xaml.cs
+++ NetworkService/NetworkService/NetworkService/MainWindow.xaml.cs
@@ -24,5 +24,77 @@ namespace NetworkService
         {
             InitializeComponent();
         }
+
+        private void GraphImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TableImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ShutdownImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    
+
+        private void ValvesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TypeAddComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TypeFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void FiltersComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Higher_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lower_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Equals_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
