1ddabf6b8447053b7e011ab6085dbdf892057552
Dragan <milosevicdragan002@gmail.com>
2024-05-29 19:05:01 +0200

    Create EntityType.cs

---
+++ NetworkService/NetworkService/NetworkService/NetworkService/NetworkService/Model/EntityType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public enum Type
+    {
+        CableSensor,
+        DigitalManometer
+    }
+    public class EntityType
+    {
+        public Type Name {  get; set; }
+        public string PicturePath {  get; set; }
+
+        public EntityType(Type name,string picturePath)
+        {
+            Name = name;
+            PicturePath = picturePath;
+
+        }
+
+    }
+}
