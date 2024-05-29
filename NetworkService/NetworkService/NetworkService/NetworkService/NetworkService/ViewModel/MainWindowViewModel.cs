aa67939646a6a3bbf0dac08ed8c7dbda39ae565b
Dragan <milosevicdragan002@gmail.com>
2024-05-29 09:52:19 +0200

    Replace Project

--- NetworkService/NetworkService/NetworkService/NetworkService/NetworkService/ViewModel/MainWindowViewModel.cs
+++ NetworkService/NetworkService/NetworkService/NetworkService/NetworkService/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NetworkService.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,17 +7,71 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using NetworkService;
+using System.Windows;
+using System.Diagnostics;
 
 namespace NetworkService.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
         private int count = 15; // Inicijalna vrednost broja objekata u sistemu
                                 // ######### ZAMENITI stvarnim brojem elemenata
                                 //           zavisno od broja entiteta u listi
+        public GraphView g;
+        public TableView t;
+
+        public ICommand MyButtonClickCommand { get; private set; }
+        private void MyButtonClick(string viewModelName)
+        {
+
+            switch (viewModelName)
+            {
+                case "GraphViewModel":
+                    CurrentView = g;
+                    break;
+                case "TableViewModel":
+                    CurrentView = t;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private UserControl currentView;
+        public UserControl CurrentView
+        {
+            get
+            {
+                return currentView;
+            }
+
+            set
+            {
+                SetProperty(ref currentView, value);
+            }
+        }
+        public MyICommand<Window> CloseWindowCommand { get; private set; }
+        private void CloseWindow(Window MainWindow)
+        {
+            foreach (var process in Process.GetProcessesByName("MeteringSimulator")) // Zameni sa stvarnim imenom aplikacije bez ekstenzije
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+            MainWindow.Close();
+        }
 
         public MainWindowViewModel()
         {
+            g = new GraphView();
+            t = new TableView();
+            CurrentView = t;
+            MyButtonClickCommand = new MyICommandWithParameter<string>(MyButtonClick);
+            CloseWindowCommand = new MyICommand<Window>(CloseWindow);
             createListener(); //Povezivanje sa serverskom aplikacijom
         }
 
