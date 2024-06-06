dda0a9d5cfd0d4cce61ad98b70f0b16785ff5563
Dragan <milosevicdragan002@gmail.com>
2024-06-06 15:05:44 +0200

    Change Folder

---
+++ MeteringSimulator/NetworkService/NetworkService/NetworkService/Model/Entity.cs
@@ -0,0 +1,55 @@
+﻿using NetworkService.ViewModel;
+using NetworkService.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public enum Type { Cable_Sensor, Digital_Manometer, All }
+    public class Entity : BindableBase
+    {
+        private int _id;
+        private string _name;
+        private Type _type;
+        private List<double> _value;
+        private List<string> _timeline;
+        private double _lastValue;
+        private string _image;
+
+        public int Id { get { return _id; } set { _id = value; OnPropertyChanged(nameof(Id)); } }
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged(nameof(Name)); } }
+        public Type Type { get { return _type; } set { _type = value; OnPropertyChanged(nameof(Type)); } }
+        public List<double> Value { get { return _value; } set { _value = value; OnPropertyChanged(nameof(Value)); } }
+        public List<string> TimelineValues { get { return _timeline; } set { _timeline = value; OnPropertyChanged(nameof(TimelineValues)); } }
+        public double LastValue { get { return _lastValue; } set { _lastValue = value; OnPropertyChanged(nameof(LastValue)); } }
+        public string Image { get { return _image; } set { _image = value; OnPropertyChanged(nameof(Image)); } }
+
+        public Entity() { }
+
+        public Entity(int id, string name, Type type, List<double> value)
+        {
+            this._id = id;
+            this._name = name;
+            this._type = type;
+            this._value = value;
+            this._timeline = new List<string> { DateTime.MinValue.ToString(), DateTime.MinValue.ToString(), DateTime.MinValue.ToString(), DateTime.MinValue.ToString(), DateTime.MinValue.ToString() };
+            if(_type == Type.Cable_Sensor)
+            {
+                Image = "C:\\Users\\milos\\Documents\\Faculty\\6. Semestar\\HCI\\Pokusaj2\\NetworkService\\NetworkService\\NetworkService\\NetworkService\\Images\\CableSensor.jpg";
+            }
+            else
+            {
+                Image = "C:\\Users\\milos\\Documents\\Faculty\\6. Semestar\\HCI\\Pokusaj2\\NetworkService\\NetworkService\\NetworkService\\NetworkService\\Images\\DigitalManometer.jpg";
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"ID: {_id} Name: {_name} Type: {_type} Value: {_value}";
+        }
+    }
+}
---
+++ MeteringSimulator/NetworkService/NetworkService/NetworkService/ViewModel/MeasurementGraphViewModel.cs
@@ -0,0 +1,216 @@
+﻿using NetworkService.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace NetworkService.ViewModel
+{
+    public class MeasurementGraphViewModel : BindableBase
+    {
+        private string _selectedEntity;
+        public string SelectedEntity { get { return _selectedEntity; } set { _selectedEntity = value; OnPropertyChanged(nameof(SelectedEntity)); } }
+        public static ObservableCollection<Entity> MeasurementGraph_entities { get; set; } = NetworkEntityViewModel.NetowrkEntities;
+        public static ObservableCollection<DateTime> Timeline_Values { get; set; } = new ObservableCollection<DateTime>();
+
+
+        private double _ellipse1 = 0;
+        private string _ellipse1Color1 = "White";
+        private double _ellipse2 = 0;
+        private string _ellipse1Color2 = "White";
+        private double _ellipse3 = 0;
+        private string _ellipse1Color3 = "White";
+        private double _ellipse4 = 0;
+        private string _ellipse1Color4 = "White";
+        private double _ellipse5 = 0;
+        private string _ellipse1Color5 = "White";
+
+        private string _timeLine_TextBox1;
+        private string _timeLine_TextBox2;
+        private string _timeLine_TextBox3;
+        private string _timeLine_TextBox4;
+        private string _timeLine_TextBox5;
+
+
+
+        public double Ellipse1 { get { return _ellipse1; } set { _ellipse1 = value; OnPropertyChanged(nameof(Ellipse1)); } }
+        public string Ellipse1Color1 { get { return _ellipse1Color1; } set { _ellipse1Color1 = value; OnPropertyChanged(nameof(Ellipse1Color1)); } }
+        public double Ellipse2 { get { return _ellipse2; } set { _ellipse2 = value; OnPropertyChanged(nameof(Ellipse2)); } }
+        public string Ellipse1Color2 { get { return _ellipse1Color2; } set { _ellipse1Color2 = value; OnPropertyChanged(nameof(Ellipse1Color2)); } }
+        public double Ellipse3 { get { return _ellipse3; } set { _ellipse3 = value; OnPropertyChanged(nameof(Ellipse3)); } }
+        public string Ellipse1Color3 { get { return _ellipse1Color3; } set { _ellipse1Color3 = value; OnPropertyChanged(nameof(Ellipse1Color3)); } }
+        public double Ellipse4 { get { return _ellipse4; } set { _ellipse4 = value; OnPropertyChanged(nameof(Ellipse4)); } }
+        public string Ellipse1Color4 { get { return _ellipse1Color4; } set { _ellipse1Color4 = value; OnPropertyChanged(nameof(Ellipse1Color4)); } }
+        public double Ellipse5 { get { return _ellipse5; } set { _ellipse5 = value; OnPropertyChanged(nameof(Ellipse5)); } }
+        public string Ellipse1Color5 { get { return _ellipse1Color5; } set { _ellipse1Color5 = value; OnPropertyChanged(nameof(Ellipse1Color5)); } }
+
+
+        public string TimeLine_TextBox1 { get { return _timeLine_TextBox1; } set { _timeLine_TextBox1 = value; OnPropertyChanged(nameof(TimeLine_TextBox1)); } }
+        public string TimeLine_TextBox2 { get { return _timeLine_TextBox2; } set { _timeLine_TextBox2 = value; OnPropertyChanged(nameof(TimeLine_TextBox2)); } }
+        public string TimeLine_TextBox3 { get { return _timeLine_TextBox3; } set { _timeLine_TextBox3 = value; OnPropertyChanged(nameof(TimeLine_TextBox3)); } }
+        public string TimeLine_TextBox4 { get { return _timeLine_TextBox4; } set { _timeLine_TextBox4 = value; OnPropertyChanged(nameof(TimeLine_TextBox4)); } }
+        public string TimeLine_TextBox5 { get { return _timeLine_TextBox5; } set { _timeLine_TextBox5 = value; OnPropertyChanged(nameof(TimeLine_TextBox5)); } }
+
+
+        private ObservableCollection<double> LastFiveValues { get; set; } = new ObservableCollection<double>();
+        private ObservableCollection<string> LastFiveDateTime { get; set; } = new ObservableCollection<string>();
+
+
+
+        public MyICommand ShowEntityCommand { get; private set; }
+
+        public MeasurementGraphViewModel()
+        {
+            ShowEntityCommand = new MyICommand(ShowEntity);
+        }
+
+        public void ShowEntity()
+        {
+            foreach (var entity in MeasurementGraph_entities)
+            {
+                string id = "";
+                if (SelectedEntity != null)
+                {
+                    id = SelectedEntity.Split(':')[1].TrimStart();
+                    id = id.Split(' ')[0].TrimStart();
+                }
+                if (entity.Id.ToString().Equals(id))
+                {
+                    UpdateValues(entity.Id); 
+
+                    if (LastFiveValues[0] / 22.7 < 0.34 || LastFiveValues[0] / 22.7 > 2.73)
+                    {
+                        Ellipse1Color1 = "Red";
+                    }
+                    else
+                    {
+                        Ellipse1Color1 = "White";
+                    }
+                    Ellipse1 = LastFiveValues[0];
+
+                    if (LastFiveValues[1] / 22.7 < 0.34 || LastFiveValues[1] / 22.7 > 2.73)
+                    {
+                        Ellipse1Color2 = "Red";
+                    }
+                    else
+                    {
+                        Ellipse1Color2 = "White";
+                    }
+                    Ellipse2 = LastFiveValues[1];
+
+                    if (LastFiveValues[2] / 22.7 < 0.34 || LastFiveValues[2] / 22.7 > 2.73)
+                    {
+                        Ellipse1Color3 = "Red";
+                    }
+                    else
+                    {
+                        Ellipse1Color3 = "White";
+                    }
+                    Ellipse3 = LastFiveValues[2];
+
+                    if (LastFiveValues[3] / 22.7 < 0.34 || LastFiveValues[3] / 22.7 > 2.73)
+                    {
+                        Ellipse1Color4 = "Red";
+                    }
+                    else
+                    {
+                        Ellipse1Color4 = "White";
+                    }
+                    Ellipse4 = LastFiveValues[3];
+
+                    if (LastFiveValues[4] / 22.7 < 0.34 || LastFiveValues[4] / 22.7 > 2.73)
+                    {
+                        Ellipse1Color5 = "Red";
+                    }
+                    else
+                    {
+                        Ellipse1Color5 = "White";
+                    }
+                    Ellipse5 = LastFiveValues[4];
+
+
+                    if (LastFiveDateTime[0] != DateTime.MinValue.ToString())
+                        TimeLine_TextBox1 = LastFiveDateTime[0];
+                    if (LastFiveDateTime[1] != DateTime.MinValue.ToString())
+                        TimeLine_TextBox2 = LastFiveDateTime[1];
+                    if (LastFiveDateTime[2] != DateTime.MinValue.ToString())
+                        TimeLine_TextBox3 = LastFiveDateTime[2];
+                    if (LastFiveDateTime[3] != DateTime.MinValue.ToString())
+                        TimeLine_TextBox4 = LastFiveDateTime[3];
+                    if (LastFiveDateTime[4] != DateTime.MinValue.ToString())
+                        TimeLine_TextBox5 = LastFiveDateTime[4];
+                }
+            }
+        }
+
+        public void UpdateValues(int id)
+        {
+            foreach (var entity in MeasurementGraph_entities)
+            {
+                if (entity.Id == id)
+                {
+                    LastFiveValues.Clear();
+                    LastFiveValues.Add(0);
+                    LastFiveValues.Add(0);
+                    LastFiveValues.Add(0);
+                    LastFiveValues.Add(0);
+                    LastFiveValues.Add(0);
+
+                    LastFiveDateTime.Clear();
+                    LastFiveDateTime.Add(DateTime.MinValue.ToString());
+                    LastFiveDateTime.Add(DateTime.MinValue.ToString());
+                    LastFiveDateTime.Add(DateTime.MinValue.ToString());
+                    LastFiveDateTime.Add(DateTime.MinValue.ToString());
+                    LastFiveDateTime.Add(DateTime.MinValue.ToString());
+
+                    //int startIndex = Math.Max(0, entity.Value.Count - 5); // Calculate the starting index for the last five values
+
+                    // Add zeros to pad the list if there are fewer than five values
+                    //int count = entity.Value.Count;
+
+                    /*int j = 4;
+                    for (int i = entity.Value.Count - 1; i <= 0; i--)
+                    {
+
+                        LastFiveValues[j--] = entity.Value[i] * 22.7;
+                    }*/
+
+                    /*int j = 4;
+                    int i = entity.Value.Count - 1;
+                    while (true)
+                    {
+                        if (j < 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            LastFiveValues[j] = entity.Value[i] * 22.7;
+                            j--;
+                            i--;
+                        }
+                    }*/
+
+                    int i = entity.Value.Count - 1;
+                    for (int j = 4; j >= 0; j--)
+                    {
+                        LastFiveValues[j] = entity.Value[i] * 22.7;
+                        i--;
+                    }
+
+                    i = entity.TimelineValues.Count - 1;
+                    for (int j = 4; j >= 0; j--)
+                    {
+                        LastFiveDateTime[j] = entity.TimelineValues[i];
+                        i--;
+                    }
+                }
+            }
+        }
+    }
+}
