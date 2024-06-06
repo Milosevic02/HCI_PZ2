04cd5931caa15cfdd5f43cdcd370a12d78c7203e
Dragan <milosevicdragan002@gmail.com>
2024-06-06 15:15:42 +0200

    Revert "Change Folder"
    This reverts commit dda0a9d5cfd0d4cce61ad98b70f0b16785ff5563.

---
+++ NetworkService/NetworkService/NetworkService/NetworkService/NetworkService/Model/EntityType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NetworkService.Model
+{
+    public enum Type
+    {
+        CableSensor,
+        DigitalManometer
+    }
+    public class EntityType
+    {
+        public Type Name {  get; set; }
+        public string PicturePath {  get; set; }
+
+        public EntityType(Type name)
+        {
+            Name = name;
+            if(name == Type.CableSensor)
+            {
+                PicturePath = "C:\\Users\\milos\\Documents\\Faculty\\6. Semestar\\HCI\\HCI_PZ2\\NetworkService\\NetworkService\\NetworkService\\NetworkService\\NetworkService\\Images\\CableSensor.jpg";
+
+            }
+            else
+            {
+                PicturePath = "C:\\Users\\milos\\Documents\\Faculty\\6. Semestar\\HCI\\HCI_PZ2\\NetworkService\\NetworkService\\NetworkService\\NetworkService\\NetworkService\\Images\\DigitalManometer.jpg";
+            }
+        }
+
+    }
+}
---
+++ NetworkService/NetworkService/NetworkService/NetworkService/NetworkService/Model/Filter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class Filter
+    {
+        public int Id { get; set; } = 0;
+        public string Operation { get; set; } = string.Empty;
+        public EntityType Type { get; set; } = null;
+
+        public Filter() { }
+
+        public bool FilterEntity(Valve valve)
+        {
+            bool retVal = true;
+            if(Operation != String.Empty)
+            {
+                switch (Operation)
+                {
+                    case "More":
+                        retVal = valve.Id > Id;
+                        break;
+                    case "Less":
+                        retVal = valve.Id < Id;
+                        break;
+                    case "Equals":
+                        retVal = valve.Id == Id;
+                        break;
+                    default:
+                        retVal = false;
+                        break;
+
+                }
+            }
+
+            if(Type != null && retVal)
+            {
+                if(!(valve.Type.Name == Type.Name))
+                {
+                    retVal = false;
+                }
+                
+            }
+
+            return retVal;
+        }
+
+        public string GetName()
+        {
+            string retVal = "";
+            if (Operation != String.Empty)
+            {
+                string id = Id.ToString();
+                switch (Operation)
+                {
+                    case "More":
+                        retVal += "More then " + id + " ";
+                        break;
+                    case "Less":
+                        retVal += "Less then " + id + " ";
+                        break;
+                    case "Equals":
+                        retVal += "Equals with  " + id + " ";
+                        break;
+                    default:
+                        retVal = "Error";
+                        break;
+                }
+            }
+
+            if (Type != null)
+            {
+                if(Type.Name == Model.Type.DigitalManometer)
+                {
+                    retVal += "Type:DM";
+                }
+                else
+                {
+                    retVal += "Type:CS";
+                }
+
+
+            }
+
+            return retVal;
+
+        }
+
+    }
+}
